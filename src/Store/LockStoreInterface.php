f3c2f73745577d2c03968f262131e76418e3ce17
Aykut Farsak <aykutfarsak@gmail.com>
2014-08-22 15:45:28 +0300

    Update LockStoreInterface.php
    `LockStoreInterface` is using in `LockManager` with `add` method, not `set`.

--- src/Store/LockStoreInterface.php
+++ src/Store/LockStoreInterface.php
@@ -3,7 +3,7 @@ namespace Metaphore\Store;
 
 interface LockStoreInterface
 {
-    public function set($key, $value, $ttl);
+    public function add($key, $value, $ttl);
 
     public function delete($key);
 }
