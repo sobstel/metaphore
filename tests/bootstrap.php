7c634bdc1eb6845dacd1ca15fe94b366832f4b9d
Przemek Sobstel <przemek@sobstel.org>
2017-04-13 18:54:58 +0200

    phpunit backward compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,8 @@
 <?php
 require __DIR__."/../vendor/autoload.php";
+
+// phpunit backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
